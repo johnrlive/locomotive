93effd008a9fe5854c410e07c8c643f392a60a69
johnrlive <john@addlocal.com>
2013-07-09 18:07:01 -0400

    start-engine.sh is create

---
+++ start-engine.sh
@@ -0,0 +1,22 @@
+# Ask if you want to build Ruby or install RVM
+  echo -e "\n"
+  echo "What OS are you using?"
+  echo "=> 1. Ubuntu"
+  echo "=> 2. OSX"
+  echo -n "Select your OS type [1 or 2]? "
+  read whichOS
+
+  if [ $whichOS -eq 1 ] ; then
+      echo -e "\n\n!!! Ubuntu !!! \n"
+      wget --no-check-certificate https://raw.github.com/johnrlive/railsready/master/railsready.sh && bash railsready.sh
+  elif [ $whichOS -eq 2 ] ; then
+      echo -e "\n\n!!! OSX !!! \n"
+      curl -O https://raw.github.com/johnrlive/railsready/master/railsready.sh && bash railsready.sh
+  else
+      echo -e "\n\n!!! Must choose to Ubuntu or OSX, exiting !!!"
+      exit 1
+  fi
+
+  echo "==> done..."
+
+  echo "\n Choo-Choo\n"
